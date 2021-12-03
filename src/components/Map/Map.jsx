378d64c5f1034f3c0d840b3d60ab7565959dd3a0
mohammed taha <mohammed@pop-os.localdomain>
2021-12-03 09:02:45 +0300

    map component completed

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
-import "./styles.js"
+import GoogleMapReact from 'google-map-react'
+import { Paper, Typography, useMediaQuery } from '@material-ui/core'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles'
 
 const Map = () => {
+    const classes = useStyles()
+    const isMobile = useMediaQuery('(min-width:600px)')
+    const coordinates = { lat: 0, lng: 0 }
+
     return (
-        <div>
-            Map
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys = {{key: "AIzaSyDQEzUdtqBwu5XRaw7gixHg72cL0jriOWw"}}
+                defaultCenter = {coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                options={""}
+                onChange={""}
+                onChildClick={""}
+            >
+
+            </GoogleMapReact>
         </div>
     )
 }
