ae38bc08217fb35a533383fee6ab4aec29fcea7e
mohammed taha <mohammed@pop-os.localdomain>
2021-12-03 06:59:07 +0300

    Header component complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
-import "./styles"
+import { Autocomplete } from "@react-google-maps/api"
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core'
+import SearchIcon  from '@material-ui/icons/Search'
+import  useStyles from './styles';
+
+
 
 const Header = () => {
+
+    const classes = useStyles();
     return (
         <div>
-            Header
+            <AppBar position="static">
+                <Toolbar className={classes.toolbar}>
+                    <Typography variant="h5" className = {classes.title}>
+                        Travel Guide
+                    </Typography>
+                    <Box display="flex">
+                    <Typography variant="h6" className = {classes.title}>
+                        Explore New Places 
+                    </Typography>
+                    {/* <Autocomplete> */}
+                        <div className = {classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase placeholder="Search..." classes = {{ root: classes.inputRoot, input: classes.inputInput}}/>
+                        </div>
+                    {/* </Autocomplete> */}
+                    </Box>
+                </Toolbar>
+            </AppBar>
         </div>
     )
 }
 
 export default Header
+
+//24.08
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import {Header, List, Map, PlaceDetails } from "../src/components";
 
 ReactDOM.render( <App /> , document.getElementById("root"));
